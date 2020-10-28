52295383dddc2da2d8d31c60bc229bce429bb279
Nicholas Reid <nicholasreid845@gmail.com>
2020-10-28 14:57:23 -0400

    added handleListFunction (non-functional)

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Shopping List App Challenge
 
 This repo contains starter files for the *Shopping List App* challenge.
+
+requirements
+enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+check and uncheck items on the list by clicking the "Check" button
+permanently remove items from the list
---
+++ index.js
@@ -0,0 +1,24 @@
+function handleListAdd() {
+    $('#js-shopping-list-form').on('submit', function(event) {
+            // prevent default of submit event
+            event.preventDefault();
+            // save value new list item as a variable
+            let listEntry = $('#shopping-list-entry').val();
+            console.log(listEntry);
+            // append listEntry to shopping list
+            $('#js-shopping-list').append(
+                `<li>
+          <span class="shopping-item">${listEntry}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>`);
+
+        })
+}
+$(handleListAdd)
