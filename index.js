c2382b916d0becca1a9b4eb33cd2c2d806a1cc7d
Nicholas Reid <nicholasreid845@gmail.com>
2020-10-28 23:03:21 -0400

    added handleDelete, ready for upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// handle adding of a list item
 function handleListAdd() {
     $('#js-shopping-list-form').on('submit', function(event) {
             // prevent default of submit event
@@ -24,14 +25,29 @@ function handleListAdd() {
         })
 }
 
+// handle checking and unchecking with the check button
 function handleCheck() {
     // on click of toggle button run function 
     $('.shopping-list').on( "click", '.shopping-item-toggle', function(event) {
         // access full li element with closest and save it as a variable
         let $li = $(this).closest('li');
         console.log($li);
+        // find specific span item and toggle class
         $li.find('span.shopping-item').toggleClass('shopping-item__checked')
+    })
+}
+
+// handle deleting list items with the delete function
+function handleDelete() {
+    // on click of delete button remove closest li element from list
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+        // save closest li to variable
+        let $li = $(this).closest('li');
+        // remove li variable
+        $($li).remove();
+      
     })
 }
 $(handleListAdd)
 $(handleCheck)
+$(handleDelete)
